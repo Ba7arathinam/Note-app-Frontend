edc1ab054bb0f5354b38b27567690ab7b38af873
Ba7arathinam <bala01225@gmail.comm>
2023-11-19 18:09:51 +0530

    redo

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
     <>
       <AppBar style={{background:'orange'}} position="sticky">
         <Toolbar>
-          <Typography variant="h4"><img src="https://1000marcas.net/wp-content/uploads/2020/10/Blogger-Logo.png" alt="" style={{maxWidth:'180px',maxHeight:'90px'}} /></Typography>
+          <Typography variant="h4"><img src="https://cdn-icons-png.flaticon.com/512/4021/4021693.png" alt="" style={{maxWidth:'180px',maxHeight:'90px'}} /></Typography>
           {isLogin && (
             <Box display={"flex"} marginLeft="auto" marginRight={"auto"}>
               <Tabs
